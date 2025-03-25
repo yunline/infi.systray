ac7867de73da9e6a8b33061186bb0dbccda35eb4
yunline <yunline@foxmail.com>
2025-03-25 21:09:45 +0800

    Don't add 'Quit' option to the menu by default

--- src/infi/systray/traybar.py
+++ src/infi/systray/traybar.py
@@ -38,7 +38,6 @@ class SysTrayIcon(object):
         self._on_quit = on_quit
 
         menu_options = menu_options or ()
-        menu_options = menu_options + (('Quit', None, SysTrayIcon.QUIT),)
         self._next_action_id = SysTrayIcon.FIRST_ID
         self._menu_actions_by_id = set()
         self._menu_options = self._add_ids_to_menu_options(list(menu_options))
